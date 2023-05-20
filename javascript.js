96cb539826ae5225b33af6d0d49db40d95618019
priscilagiacopini <130710358+priscilagiacopini@users.noreply.github.com>
2023-05-20 18:59:52 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,14 @@
+let name= "Priscila"; //String
+let surname= "Giacopini"; //String
+
+console.log (name + " " + surname);
+
+let numberone= 20;
+let numbertwo= 17;
+
+let resultsuma= numberone + numbertwo;
+console.log ("Suma:", resultsuma);
+
+let suma= 21 + 18;
+let nuevaVariable= suma + 20;
+console.log ("Nueva suma", suma);
